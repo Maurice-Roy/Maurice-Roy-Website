c0cb74bbb8959a0f67b63c4c0b787f8753ed2abe
Maurice Roy <mautayro@gmail.com>
2018-04-30 13:51:33 -0400

    stubbed design & audio pages - need to fix scrolling on page load

--- src/components/AudioPage.js
+++ src/components/AudioPage.js
@@ -5,7 +5,11 @@ class AudioPage extends React.Component {
   render () {
     return (
       <div id="AudioPage" onClick={() => this.props.changeDisplay("LandingPage")}>
-
+        <p id="Clips">
+          <span className="Line">━━━━━━━━</span><br/><br/>
+          ¯\_(ツ)_/¯ WHOA! THERE'S NOTHING HERE YET! ¯\_(ツ)_/¯<br/><br/>
+          <span className="Line">━━━━━━━━</span>
+        </p>
       </div>
     )
   }
--- src/components/DesignPage.js
+++ src/components/DesignPage.js
@@ -5,7 +5,11 @@ class DesignPage extends React.Component {
   render () {
     return (
       <div id="DesignPage" onClick={() => this.props.changeDisplay("LandingPage")}>
-
+        <p id="Images">
+          <span className="Line">━━━━━━━━</span><br/><br/>
+          ¯\_(ツ)_/¯ WHOA! THERE'S NOTHING HERE YET! ¯\_(ツ)_/¯<br/><br/>
+          <span className="Line">━━━━━━━━</span>
+        </p>
       </div>
     )
   }
